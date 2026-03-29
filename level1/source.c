108c5919d69fb024927c269d849a0129bb174fd5
Diego Gerwig <diegogerwig@gmail.com>
2026-03-29 22:56:29 +0200

    update

--- level1/source.c
+++ level1/source.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(void)
+{
+	char user_input[100];
 
+	printf("Please enter key: ");
 
+	scanf("%s", user_input);
+
+	if (strcmp(user_input, "__stack_check") == 0)
+	{
+		printf("Good job.\n");
+	}
+	else
+	{
+		printf("Nope.\n");
+	}
+
+	return 0;
+}
