cbbab9f10f83efa809b3dd24bc373bbc5e810a34
Diego Gerwig <diegogerwig@gmail.com>
2026-03-31 16:28:37 +0200

    update

--- level2/source.c
+++ level2/source.c
@@ -8,6 +8,7 @@ int main(void)
 	char output[9];
 	char tmp[4];
 	int i, idx;
+	char *secret= "delabere";
 
 	printf("Please enter key: ");
 	if (scanf("%23s", input) != 1)
@@ -16,29 +17,33 @@ int main(void)
 		exit(1);
 	}
 
-	if (input[1] != '0' || input[0] != '0')
+	if (input[0] != '0' || input[1] != '0')
 	{
 		puts("Nope.");
 		exit(1);
 	}
 
-	memset(output, 0, 9);
-	output[0] = 'd';
-	i = 2;
-	idx = 1;
+	memset(output, 0, 9); 
+	output[0] = 'd';	  
 
-	while (strlen(output) < 8 && i < (int)strlen(input))
+	int position_read = 2;   
+	int position_escritura = 1; 
+
+	while (strlen(output) < 8 && position_read < (int)strlen(input))
 	{
-		tmp[0] = input[i];
-		tmp[1] = input[i + 1];
-		tmp[2] = input[i + 2];
-		tmp[3] = '\0';
-		output[idx] = (char)atoi(tmp);
-		i += 3;
-		idx += 1;
+		tmp[0] = input[position_read];
+		tmp[1] = input[position_read + 1];
+		tmp[2] = input[position_read + 2];
+		tmp[3] = '\0'; 
+
+		int codigo_ascii = atoi(tmp);
+		output[position_escritura] = (char)codigo_ascii;
+
+		position_read += 3;	
+		position_escritura += 1; 
 	}
 
-	if (strcmp(output, "delabere") == 0)
+	if (strcmp(output, secret) == 0)
 		puts("Good job.");
 	else
 	{
